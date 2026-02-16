692d71241f15cb1670116deb0901ebc2fc3184e9
Andre Fonseca <andrefonseca.aff@gmail.com>
2026-02-16 13:15:34 +0000

    Connect MongoDB to Node js

---
+++ server/server.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const app = require("./index");
+
+dotenv.config();
+
+// mongoDB connection
+mongoose
+  .connect(
+    process.env.MONGODB_URI.replace(
+      "<DB_PASSWORD>",
+      process.env.DATABASE_PASSWORD,
+    ),
+  )
+  .then(() => console.log("MongoDB connected"))
+  .catch((error) => {
+    console.error("MongoDB connection failed:", error);
+  });
+
+const port = process.env.PORT || 5001;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
