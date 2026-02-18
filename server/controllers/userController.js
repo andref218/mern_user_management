96245ce4d1303927ea25cdae3e3619fdd6bd352f
Andre Fonseca <andrefonseca.aff@gmail.com>
2026-02-18 16:48:11 +0000

    Implement user listing, get by id and create endpoints

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -20,10 +20,8 @@ exports.searchUsers = async (req, res) => {
   try {
     const query = req.params.query;
     const page = parseInt(req.query.page) || 1;
-
     // Number of users per page (default: 10)
     const limit = parseInt(req.query.limit) || 10;
-
     const skip = (page - 1) * limit;
 
     // MongoDB search query:
@@ -51,6 +49,80 @@ exports.searchUsers = async (req, res) => {
       totalPages: Math.ceil(total / limit),
       totalUsers: total,
     });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error Fetch statistics", error: error.message });
+  }
+};
+
+//Get all Users
+exports.getAllUsers = async (req, res) => {
+  try {
+    const page = parseInt(req.query.page) || 1;
+    // Number of users per page (default: 10)
+    const limit = parseInt(req.query.limit) || 10;
+    const skip = (page - 1) * limit;
+
+    // Fetch users from database with sorting and pagination
+    const users = await User.find()
+      .sort({ createdAt: -1 })
+      .skip(skip)
+      .limit(limit);
+
+    const total = await User.countDocuments();
+
+    res.json({
+      users,
+      currentPage: page,
+      totalPages: Math.ceil(total / limit),
+      totalUsers: total,
+    });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error Fetch statistics", error: error.message });
+  }
+};
+
+//Get Single User
+exports.getUserById = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const user = await User.findById(id);
+    if (!user) return res.status(404).json({ message: "User not found" });
+    res.json(user);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error Fetch statistics", error: error.message });
+  }
+};
+
+//Create User
+exports.createUser = async (req, res) => {
+  try {
+    // Destructure the user data from the request body
+    const { name, email, phone, status } = req.body;
+    if (!name || !email || !phone)
+      return res
+        .status(400)
+        .json({ message: "Name, email and phone are required" });
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser)
+      return res.status(400).json({ message: "Email already exist" });
+
+    const user = new User({
+      name,
+      email,
+      phone,
+      status: status || "Active",
+    });
+
+    await user.save();
+    res.status(201).json({ user, message: "User successfully created" });
   } catch (error) {
     res
       .status(500)
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const { model } = require("mongoose");
-const { getStatus, searchUsers } = require("../controllers/userController");
+const {
+  getStatus,
+  searchUsers,
+  getUserById,
+  getAllUsers,
+  createUser,
+} = require("../controllers/userController");
 
 const router = express.Router();
 
 router.get("/status", getStatus);
 router.get("/search/:query", searchUsers);
+router.get("/", getAllUsers);
+router.get("/:id", getUserById);
+router.post("/", createUser);
 
 module.exports = router;
